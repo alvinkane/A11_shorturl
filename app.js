715003538c51f250d6bd8a87eaa13fe7a6a94036
alvin <ckuvn1854@gmail.com>
2023-03-31 16:29:27 +0800

    feat: connect to mongodb

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 const app = express();
+mongoose.connect(process.env.MONGODB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+const db = mongoose.connection;
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+db.once("open", () => {
+  console.log("mongodb connected");
+});
 
 const port = 3000;
 
@@ -16,6 +33,8 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+// 縮短網址
 
 // 監聽
 app.listen(port, () => {
