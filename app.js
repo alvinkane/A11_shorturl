0ed05b32537eaf360e9b128e7b6e4d9df9cdf7e9
alvin <ckuvn1854@gmail.com>
2023-03-31 14:40:45 +0800

    feat: use hbs and add index.hbs

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 
 const app = express();
 
 const port = 3000;
 
+// template
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }));
+app.set("view engine", "hbs");
+
+// 首頁
 app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
+  res.render("index");
 });
 
+// 監聽
 app.listen(port, () => {
   console.log(`this is running on http://localhost:${port}`);
 });
